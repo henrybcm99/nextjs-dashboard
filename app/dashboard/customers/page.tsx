d0fcf2c84d6a5a5e339f3ee7dc18446b78637d24
henrybcm99 <henrybcm99@gmail.com>
2024-09-06 22:30:06 +0200

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CustomersPage = () => {
+  return (
+    <div>Customers</div>
+  )
+}
+export default CustomersPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+  return (
+    <div>invoices</div>
+  )
+}
+export default InvoicesPage
